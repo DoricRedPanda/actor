aa1c638585ef444845ed728bc59d5a9a7836cf69
DoricRedPanda <azatoth@ro.ru>
2021-04-18 10:59:40 +0600

    add examples

---
+++ examples/array.c
@@ -0,0 +1,24 @@
+int a[10];
+
+int
+main()
+{
+	a[3] = 42;
+	a[0] = 1;
+	write a[3] + a[0];
+	/* WARNING: code below is bad for your health */
+	/* mental health that is*/
+
+
+
+
+
+
+
+
+
+
+	a[a[0]] = a[2 * a[0] + 1] * 42;
+	write a[1];
+}
+
---
+++ examples/fibonacci_array.c
@@ -0,0 +1,19 @@
+int n, i, fib[256];
+
+int
+main()
+{
+	n = 30;
+	fib[0] = 0;
+	fib[1] = 1;
+	if (n < 0 || n >= 256) {
+		write -1;
+	} else {
+		i = 1;
+		while (i < n) {
+			fib[i + 1] = fib[i] + fib[i - 1];
+			i = i + 1;
+		}
+		write fib[i];
+	}
+}
